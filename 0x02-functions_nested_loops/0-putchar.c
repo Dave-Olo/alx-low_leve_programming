82106a641ba1e763f8ea48470b135b907f3a3bb7
Dave-Olo <destineddave@gmail.com>
2022-03-15 20:11:01 +0100

    Putchar print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+	char c[] = "_putchar";
+	
+	int i = 0;
+	
+	for(i=0; i < 9; i++)
+		_putchar(c[i]);
+		
+
+	}
+	_putchar('\n');
+	return(0)
+}
+
+
