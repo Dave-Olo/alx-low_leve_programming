31e79ecf2acc3e21096e92c4d187129a406a42bb
Dave-Olo <destineddave@gmail.com>
2022-03-16 09:55:06 +0100

    Debugging

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/* positive_or_negative - print */
+void positive_or_negative(int i);
+
+/* largest_number - return the largest number. */
+int largest_number(int, int, int);
+
+/* print_remaining_days - print days to end of year. */
+void print_remaining_days(int, int, int);
+
+/* covert_day - return days*/
+int convert_day(int, int);
+#endif /* MAIN_H */
