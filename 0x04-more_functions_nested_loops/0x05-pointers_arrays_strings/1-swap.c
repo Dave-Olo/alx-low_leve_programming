eea54a3abc7f4e694b51cd3e191fa764d3a0c22c
Dave-Olo <destineddave@gmail.com>
2022-03-21 10:12:49 +0100

    Arrays, Pointers & Strings

---
+++ 0x04-more_functions_nested_loops/0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers.
+ *
+ * @a: pointer to number to be changed
+ * @b: pointer to number to be changed
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int save;
+
+	hold = *a;
+	*a  = *b;
+	*b = save;
+}
---
+++ 0x04-more_functions_nested_loops/0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This teaches about pointers, arrays and strings in C programming.
