094279a01aea325851db64e635df38e7dcabdaa8
Dave-Olo <destineddave@gmail.com>
2022-04-01 11:23:44 +0100

    Advanced argc Argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - enter point.
+  *
+  * @argc: number of arguments passed in call to program
+  * @argv: array of arguments
+  *
+  * Return: 0 if success, or 1 if error
+  */
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+	char *texto;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			texto = argv[i];
+
+			for (j = 0; *(texto + j) != '\0'; j++)
+			{
+				if (!('0' <= *(texto + j) && *(texto + j) <= '9'))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(argv[i]);
+		}
+
+		printf("%d\n", sum);
+
+	}
+
+	return (0);
+
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is about argc which shows the number of command line argument  and argv which shows an array containing the program command line arguments.
