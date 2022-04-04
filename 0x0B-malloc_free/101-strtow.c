3f6d4be7897c334587d56fccf592ebc028e5838e
Dave-Olo <destineddave@gmail.com>
2022-04-04 13:23:37 +0100

    Advanced malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,41 +1,28 @@
-#include "main.h"
+#include  <main.h>
 #include <stdlib.h>
-#include <stdio.h>
-int _strlen(char *s);
 /**
-* strtow - function
-* @str: string to split
-*
-* Description: function to split a string into words
-* Return: pointer or NULL if fail
-*/
+ * strtow - splits a string into words.
+ *
+ * @str: string to be splited.
+ *
+ * Return: ponter to array of words
+ */
 char **strtow(char *str)
 {
-	char **split;
+	int count_p;
+	char *s;
+	int i, j;
 
-	split = malloc(_strlen(str) * sizeof(char *));
-	if (str == NULL)
+	if (str == NULL || str == "")
 		return (NULL);
-	if (split == NULL)
-		return (NULL);
 
-	return (split);
-}
-/**
-* _strlen - function
-* @s: first operand & pointer
-*
-* Description: function that returns the length of a string
-* Return: Always 0
-*/
-int _strlen(char *s)
-{
-	int index = 0;
-
-	while (*s != '\0')
+	for (count_p = 0; str[count_p]; count_p++)
 	{
-		index++;
-		s++;
+		if (str[count_p] == ' ')
+			count_p++;
 	}
-	return (index);
+
+	s = malloc(sizeof(char *) * count_p + 1);
+	if (s == NULL)
+		return (NULL);
 }
