b438aff5f82681234ede5026d33c486100691b3f
Dave-Olo <destineddave@gmail.com>
2022-04-07 12:22:20 +0100

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -57,3 +57,50 @@ void print_number_string(char *array_respuesta, int len)
 		_putchar('0');
 	_putchar('\n');
 }
+
+/**
+ * main - enter point
+ *
+ * @argc: number of parameters passed to program
+ * @argv: array of parameters
+ *
+ * Return: 0 on succes
+ */
+int main(int argc, char *argv[])
+{
+	int len1 = 0, len2 = 0, sum = 0, index1, index2, i;
+	char *num1, *num2, *array_respuesta;
+
+	if (argc != 3)
+		exit_x_error();
+
+	num1 = argv[1];
+	num2 = argv[2];
+	len1 = number_str_len(num1);
+	len2 = number_str_len(num2);
+
+	array_respuesta = malloc(sizeof(char) * (len1 + len2));
+	if (array_respuesta == NULL)
+		exit_x_error();
+
+	/* multiply the numbers */
+	index1 = len1 - 1;
+	for (i = len1 + len2 - 1; i > 0; i--, index1--)
+	{
+		for (index2 = 0; index2 < len2; index2++)
+		{
+			if (index2 < (len1 - index1) && (index1 + index2) >= 0)
+				sum += (num1[index1 + index2] - '0') * (num2[len2 - index2 - 1] - '0');
+		}
+		array_respuesta[i] = (sum % 10) + '0';
+		sum /= 10;
+	}
+	if (sum != 0)
+		array_respuesta[i] = sum + '0';
+
+	/* print the number*/
+	print_number_string(array_respuesta, (len1 + len2));
+
+	free(array_respuesta);
+	return (0);
+}
