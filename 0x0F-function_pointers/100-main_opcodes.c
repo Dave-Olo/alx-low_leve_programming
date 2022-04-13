204bc499a8c2f31c5c7aff4682aaf26776d79d04
Dave-Olo <destineddave@gmail.com>
2022-04-13 10:54:43 +0100

    Advanced Function Pointers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * main - prgogram that prints the opcodes of its own main function.
+ *
+ * @argc: number of elements in argv.
+ * @argv: array of arguments.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int (*fmain)(int, char **) = &main;
+	int i;
+	char *ptr = (char *)fmain;
+	int orden;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < atoi(argv[1]); i++)
+	{
+		orden = ptr[i];
+		if (i != 0)
+			printf(" ");
+
+		if (orden < 0)
+			orden += 256;
+
+		if (orden < 16)
+			printf("0");
+
+		printf("%x", orden);
+	}
+
+	printf("\n");
+	return (0);
+}
