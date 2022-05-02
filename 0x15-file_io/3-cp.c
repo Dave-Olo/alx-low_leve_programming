8cbe7f12419c14055be9e459e348f86437087dc0
Dave-Olo <destineddave@gmail.com>
2022-05-02 18:12:34 +0100

    Copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,86 +1,110 @@
-#include "main.h"
-void cant_write(char *s);
-void cant_read(char *s);
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define BUFSIZE 1024
+
 /**
-* main - entry point!
-* @s: string to read/writey
-*
-* Description: cp file_from file_to
-* Return: Always 0
-*/
-int main(int argc, char *argv[])
+ * _close - close a file descriptor and print an error message upon failure
+ * @fd: the file descriptor to close
+ *
+ * Return: 0 upon success, -1 upon failure
+ */
+int _close(int fd)
 {
-	char *buff[1024];
-	int fd1, fd2;
-	ssize_t rd, wr;
+	if (!close(fd))
+		return (0);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	return (-1);
+
+}
+
+/**
+ * _read - read from a file and print an error message upon failure
+ * @filename: the name of the file to read from
+ * @fd: the file descriptor to read from
+ * @buf: the buffer to write to
+ * @count: the number of bytes to read
+ *
+ * Return: The number of bytes read, or -1 upon failure
+ */
+ssize_t _read(const char *filename, int fd, char *buf, size_t count)
+{
+	ssize_t bytes_read = read(fd, buf, count);
+
+	if (bytes_read > -1)
+		return (bytes_read);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+	return (-1);
+}
+
+/**
+ * _write - write to a file and print an error message upon failure
+ * @filename: the name of the file to write to
+ * @fd: the file descriptor to write to
+ * @buf: the buffer to read from
+ * @count: the number of bytes to write
+ *
+ * Return: The number of bytes written, or -1 upon failure
+ */
+ssize_t _write(const char *filename, int fd, const char *buf, size_t count)
+{
+	ssize_t bytes_written = write(fd, buf, count);
+
+	if (bytes_written > -1)
+		return (bytes_written);
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+	return (-1);
+}
+
+/**
+ * main - copy a file's contents to another file
+ * @argc: the argument count
+ * @argv: the argument values
+ *
+ * Return: Always 1
+ */
+int main(int argc, const char *argv[])
+{
+	int fd_in, fd_out;
+	ssize_t bytes_read;
+	char buffer[BUFSIZE];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	if (argv[1] == NULL)
+	fd_in = open(argv[1], O_RDONLY);
+	if (fd_in < 0)
 	{
-		cant_read(argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd1 = open(argv[1], O_RDONLY);
-	if (fd1 == -1)
-		cant_read(argv[1]);
-
-	fd2 = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd2 == -1)
-		cant_write(argv[2]);
-
-	rd = read(fd1, buff, 1024);
-	if (rd == -1)
-		cant_read(argv[1]);
-
-	wr = write(fd2, buff, rd);
-	if (wr == -1)
-		cant_write(argv[2]);
-	while (rd == 1024)
+	fd_out = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_out < 0)
 	{
-		rd = read(fd1, buff, 1024);
-		if (rd == -1)
-			cant_read(argv[1]);
-		wr = write(fd2, buff, rd);
-		if (wr == 1)
-			cant_write(argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		_close(fd_in);
+		exit(99);
 	}
-	if (close(fd1) < 0)
+	while ((bytes_read = _read(argv[1], fd_in, buffer, BUFSIZE)))
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		if (bytes_read < 0)
+		{
+			_close(fd_in);
+			_close(fd_out);
+			exit(98);
+		}
+		if (_write(argv[2], fd_out, buffer, bytes_read) < 0)
+		{
+			_close(fd_in);
+			_close(fd_out);
+			exit(99);
+		}
+	}
+	if ((_close(fd_in) | _close(fd_out)) < 0)
 		exit(100);
-	}
-	if (close(fd2) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
-		exit(100);
-	}
 	return (0);
-}
-/**
-* cant_read - function
-* @s: string to read
-*
-* Description: error function if unable to read
-* Return: 0
-*/
-void cant_read(char *s)
-{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
-	exit(98);
-}
-/**
-* cant_write - function
-* @s: string to write
-*
-* Description: error func if unable to write
-* Return: 0
-*/
-void cant_write(char *s)
-{
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
-	exit(99);
 }
